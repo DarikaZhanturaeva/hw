9f7093c05580bccd18f5c96c2ba43b8219aed577
Darika <darikazhanturaeva@gmail.com>
2024-05-30 00:16:35 +0600

    hw1_1m

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double[] numbers = {6.3, 5.3, -9.3, 9.2, 3.7, 9.4, -2.4, 7.2, -5.2, 11.4, -4.2, -8.4, 9.3, -8.4, 2.4};
-        boolean negative = false;
-        double sum = 0;
-        int count = 0;
+        String myAge;
+        final int NUM = 50;
+        String world = "World";
+        myAge = NUM + world;
+        String name = "Введите ваше имя:";
+        System.out.print(myAge);
+        System.out.print(NUM);
+        System.out.println(world);
 
-        for (double i : numbers) {
-            if (negative && i > 0) {
-                sum += i;
-                count++;
-            }
-            if (i < 0) {
-                negative = true;
-            }
-        }
-
-        double avg = sum / count;
-        System.out.println("Average arithmetic: " + avg);
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to check for a palindrome:");
-        String input = scanner.nextLine();
-        String cleanInput = input.replaceAll("\\s", "").toLowerCase();
-        if (isPalindrome(cleanInput)) {
-            System.out.println("The entered string is a palindrome.");
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
         } else {
-            System.out.println("The entered string is not a palindrome.");
+            System.out.println("“Вы сохранили ноль");
         }
-    }
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        //dop
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(name);
+        String name1 = scanner.next();
+        System.out.println("Здравствуй " + name1);
     }
 }
